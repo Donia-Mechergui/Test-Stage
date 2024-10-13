f91aae26cf47203b643f11537c6e1f590fa6c508
Mechergui Donia <doniamechergui55@gmail.com>
2024-10-13 17:39:26 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"",component:HomeComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"about",component:AboutComponent},
+  {path:"pricing",component:PricingComponent},
+  {path:"product",component:ProductComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProductComponent } from './components/product/product.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { NewsLetterComponent } from './components/news-letter/news-letter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    ProductComponent,
+    AboutComponent,
+    HomeComponent,
+    PricingComponent,
+    BannerComponent,
+    FeaturesComponent,
+    NewsLetterComponent
   ],
   imports: [
     BrowserModule,
